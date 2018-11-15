1571b655a870f664f5aeeb2fd8660a00cd4a4578
Thomas Willey <tomwilleyonline@hotmail.com>
2018-11-15 13:02:00 -0600

    including setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='simpledl',
+    version='0.1.0',
+    url='https://github.com/thomaswilley/pysimpledl',
+    author='@thomaswilley',
+    description=('A simple deep learning package with minimal dependencies.'),
+    license='AGPL3',
+    py_modules=['simpledl'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
