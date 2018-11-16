a696165a4b41a325b2a761f45892c2c854acbb3f
Thomas Willey <tomwilleyonline@hotmail.com>
2018-11-16 10:15:40 -0600

    attempting to fix broken pkg

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='simpledl',
@@ -7,7 +7,7 @@ setup(
     author='@thomaswilley',
     description=('A simple deep learning package with minimal dependencies.'),
     license='AGPL3',
-    py_modules=['simpledl'],
+    keywords='AI ML DL machinelearning',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
@@ -16,4 +16,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['numpy'],
 )
